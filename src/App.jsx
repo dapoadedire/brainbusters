c123f315cf3538df53378c3fe5d5e31f8a73b409
dapoadedire <adedireadedapo19@gmail.com>
2023-01-15 18:16:29 +0100

    feat: finally done with the mvp

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
+      <div>
         <QuizHome />
       </div>
     </QueryClientProvider>
--- src/components/QuizHome.jsx
+++ src/components/QuizHome.jsx
@@ -1,63 +1,100 @@
 import { useState } from "react";
 import QuizBank from "./QuizBank";
 import _ from "lodash";
-import parse from "html-react-parser";
-import AddID from "../helpers/AddId";
-import OptionButtons from "./OptionButtons";
+// import parse from "html-react-parser";
+
+const quizzes = QuizBank.results.map((question, index) => {
+  question.id = index + 1;
+  question.all_options = _.shuffle([
+    question.correct_answer,
+    ...question.incorrect_answers,
+  ]);
+  return question;
+});
 
 const QuizHome = () => {
-  const quizzes = AddID(QuizBank).results;
-  const [index, setIndex] = useState(0);
-  const currentQuiz = quizzes[index];
+  const [activeQuiz, setActiveQuiz] = useState(0);
+  const [score, setScore] = useState(0);
+  const currentQuiz = quizzes[activeQuiz];
+  const [selected, setSelected] = useState(null);
+  const [showResult, setShowResult] = useState(false)
 
-  function handleOptionClick(answer) {
-    console.log("Answer selected: ", answer);
+  function nextQuiz() {
+    setSelected(null);
+    if (activeQuiz !== quizzes.length - 1){
+      setActiveQuiz((p) => p + 1);
+    }
+    else{
+      setActiveQuiz(0)
+      setShowResult(true);
+    }
   }
 
-  const answers = [
-    currentQuiz.correct_answer,
-    ...currentQuiz.incorrect_answers,
-  ];
-  const shuffledAnswers = _.shuffle(answers);
+  const {
+    category,
+    type,
+    difficulty,
+    question,
+    correct_answer,
+    id,
+    all_options,
+  } = currentQuiz;
 
-  function handleNextQuestion() {
-    setIndex(index + 1);
-  }
+  console.log(activeQuiz, quizzes.length);
+  const gradeAnswer = (answer, index) => {
+    setSelected(index);
+    if (answer === correct_answer) {
+      setScore((x) => x + 1);
 
-  function submitQuiz() {
-    console.log("Quiz Submitted");
-  }
+      console.log("right");
+    } else {
+      setScore((x) => x + 0);
+
+      console.log("wrong");
+    }
+  };
+
+  const addLeadingZero = (number) => (number > 9 ? number : `0${number}`);
 
   return (
-    <div>
-      <div></div>
-      <h1>Quiz Home</h1>
-
-      <div>
-        <p>
-          Question {currentQuiz.id} of {quizzes.length}
-        </p>
-        <h2>{parse(currentQuiz.question)}</h2>
-        {currentQuiz.id === quizzes.length ? (
-          <button onClick={submitQuiz}>Finish</button>
-        ) : (
-          <button
-            onClick={() => {
-              handleNextQuestion();
-            }}
-          >
-            Next Question
-          </button>
-        )}
-
+ <div className="container">
+  {!showResult ? (
         <div>
-          <OptionButtons
-            shuffledAnswers={shuffledAnswers}
-            handleOptionClick={handleOptionClick}
-          />
+          <p>Score: {score}</p>
+          <p>
+            Question {addLeadingZero(id)} of {addLeadingZero(quizzes.length)}
+          </p>
+          <p>Category: {category}</p>
+          <p>Type: {type}</p>
+          <p>Difficulty: {difficulty}</p>
+
+          <h2>Question: {question}</h2>
+
+          <div>
+            {all_options.map((option, index) => (
+              <button key={index} onClick={() => gradeAnswer(option, index)}
+                className={`${selected === index ? 'bg-blue-500 text-white' : ''}`}
+              >
+                {option}
+              </button>
+            ))}
+          </div>
+          <button onClick={nextQuiz} disabled={selected === null} className="bg-red-500 p-5 disabled:bg-red-300">
+            {activeQuiz === quizzes.length - 1 ? "Finish" : "Next"}
+          </button>
         </div>
-      </div>
+  ): (
+    <div>
+            <h3>Result</h3>
+            <p>
+              Total Question: <span>{quizzes.length}</span>
+            </p>
+            <p>
+              Total Score:<span> {score}</span>
+            </p>
     </div>
+  )}
+ </div>
   );
 };
 
